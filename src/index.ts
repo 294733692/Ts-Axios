65b81b68c7c46e9ce0ab356b9512f51935826dbe
yu <18113406084@163.com>
2019-09-20 10:27:46 +0800

    axios config url commit

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
+import { buildURL } from './helpers/url'
 
 function axios(config: AxiosRequestConfig): void {
   // TODO
+  precessConfig(config)
   xhr(config)
+}
+
+// 处理config参数
+function precessConfig(config: AxiosRequestConfig): void {
+  config.url = transformURL(config)
+}
+
+// 处理config的url做处理
+function transformURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
 }
 
 export default axios
