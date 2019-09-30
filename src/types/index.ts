0f4df74aabfc7f885afbe99248db35e908b5e9b9
yu <18113406084@163.com>
2019-09-30 13:52:25 +0800

    返回响应数据处理修改返回为泛型

--- src/types/index.ts
+++ src/types/index.ts
@@ -26,8 +26,8 @@ export interface AxiosRequestConfig {
 }
 
 // 返回接口类型
-export interface AxiosResponse {
-  data: any
+export interface AxiosResponse<T = any> {
+  data: T
   status: number
   statusText: string
   headers: any
@@ -35,7 +35,7 @@ export interface AxiosResponse {
   request: any
 }
 
-export interface AxiosPromise extends Promise<AxiosResponse> {}
+export interface AxiosPromise<T = any> extends Promise<AxiosResponse<T>> {}
 
 // 扩展Error接口
 export interface AxiosError extends Error {
@@ -48,21 +48,21 @@ export interface AxiosError extends Error {
 
 // axios公共方法，扩展接口类型
 export interface Axios {
-  request(config: AxiosRequestConfig): AxiosPromise
+  request<T = any>(config: AxiosRequestConfig): AxiosPromise<T>
 
-  get(url: string, config?: AxiosRequestConfig): AxiosPromise
+  get<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
 
-  delete(url: string, config?: AxiosRequestConfig): AxiosPromise
+  delete<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
 
-  head(url: string, config?: AxiosRequestConfig): AxiosPromise
+  head<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
 
-  options(url: string, config?: AxiosRequestConfig): AxiosPromise
+  options<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
 
-  post(url: string, data: any, config?: AxiosRequestConfig): AxiosPromise
+  post<T = any>(url: string, data: any, config?: AxiosRequestConfig): AxiosPromise<T>
 
-  put(url: string, data: any, config?: AxiosRequestConfig): AxiosPromise
+  put<T = any>(url: string, data: any, config?: AxiosRequestConfig): AxiosPromise<T>
 
-  patch(url: string, data: any, config?: AxiosRequestConfig): AxiosPromise
+  patch<T = any>(url: string, data: any, config?: AxiosRequestConfig): AxiosPromise<T>
 }
 
 // 混合对象类型接口
