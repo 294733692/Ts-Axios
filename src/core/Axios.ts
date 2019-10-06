1c1732d458dfc815fe66c793ee92c5e38736a55d
余云超 <18113406084@163.com>
2019-10-06 14:18:21 +0800

    拦截器添加和实现,实现拦截器链式调用

--- src/core/Axios.ts
+++ src/core/Axios.ts
@@ -1,7 +1,28 @@
-import { AxiosPromise, AxiosRequestConfig, Method } from '../types'
+import {AxiosPromise, AxiosRequestConfig, AxiosResponse, Method, RejectedFn, ResolvedFn} from '../types'
 import dispatchRequest from './dispatchRequest'
+import interceptorManager from "./interceptorManager";
+
+interface Interceptors {
+  request: interceptorManager<AxiosRequestConfig>,
+  response: interceptorManager<AxiosResponse>
+}
+
+interface PromiseChain<T> {
+  resolved: ResolvedFn<T> | ((config: AxiosRequestConfig) => AxiosPromise)
+  rejected?: RejectedFn
+}
 
 export default class Axios {
+  interceptors: Interceptors
+
+  constructor() {
+    this.interceptors = {
+      // 用户自己添加拦截器，例如this.interceptors.request/response.use
+      request: new interceptorManager<AxiosRequestConfig>(),
+      response: new interceptorManager<AxiosResponse>()
+    }
+  }
+
   request(url: any, config?: any): AxiosPromise {
     // 支持接口类型AxiosInstance第二种，参数变为any，在运行的时候做判断
     if (typeof url === 'string') {
@@ -14,7 +35,33 @@ export default class Axios {
       // 未传入url，满足接口类型第一种类型
       config = url
     }
-    return dispatchRequest(config)
+
+    // 定义request链
+    const chain: PromiseChain<any>[] = [{  // 用于存放一堆拦截器和初始值
+      resolved: dispatchRequest,
+      rejected: undefined
+    }]
+
+    // 添加request拦截器
+    this.interceptors.request.forEach(interceptor => {
+      // 添加的拦截器要先执行
+      chain.unshift(interceptor)
+    })
+
+    // 添加response拦截器
+    this.interceptors.response.forEach(interceptor => {
+      // 先添加的后执行
+      chain.push(interceptor)
+    })
+
+    let promise = Promise.resolve(config)
+
+    while (chain.length) {
+      const {resolved, rejected} = chain.shift()!  // chain.shift可能为promise.chain类型，也可能为空，类型断言shift不为空
+      promise = promise.then(resolved, rejected)
+    }
+
+    return promise
   }
 
   get(url: string, config: AxiosRequestConfig): AxiosPromise {
