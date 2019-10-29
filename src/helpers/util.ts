5c93d5d8c799741b00026097c417b970179ece33
yu <18113406084@163.com>
2019-10-29 11:31:24 +0800

    uitl单元测试，测试通过，覆盖率100%

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -1,7 +1,4 @@
 // 工具方法、辅助函数
-
-import { Method } from '../types'
-
 const toString = Object.prototype.toString
 
 // 判断是否是日期, 返回值使用ts类型保护
@@ -9,11 +6,11 @@ export function isDate(val: any): val is Date {
   return toString.call(val) === '[object Date]'
 }
 
-// 判断是否是对象，返回值使用ts类型保护
-export function isObject(val: any): val is Object {
-  // typeof null 会返回object，判断除不等于null的情况
-  return val !== null && typeof val === 'object'
-}
+// // 判断是否是对象，返回值使用ts类型保护
+// export function isObject(val: any): val is Object {
+//   // typeof null 会返回object，判断除不等于null的情况
+//   return val !== null && typeof val === 'object'
+// }
 
 // 判断是否是普通对象，返回值使用ts类型保护
 export function isPlainObject(val: any): val is Object {
